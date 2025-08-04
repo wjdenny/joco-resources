a46c0325097dd88b8ff20467c129d252d09d419b
Will Denny <will@wjdenny.com>
2025-08-04 01:51:46 -0500

    consildate data attributes

--- js/search.js
+++ js/search.js
@@ -1,16 +1,16 @@
 const searchForm = document.querySelector("form[role='search']")
 
 const resources = document.querySelectorAll("#resources article")
-const data = [ ...resources ].map(e => ({
-    key: e.dataset.key,
-    title: e.querySelector("h5").dataset.resourceTitle,
-    note: e.querySelector("[data-resource-note]").dataset.resourceNote
-}))
+const data = [ ...resources ].map(e => e.dataset)
 
 const idx = lunr(function () {
     this.ref('key');
-    this.field('title')
-    this.field('note')
+    this.field('title');
+    this.field('note');
+    this.field('tags');
+
+    this.k1(1.2);
+    this.b(0);
 
     data.forEach(function (resource) { this.add(resource) }, this)
 })
@@ -22,6 +22,7 @@ searchForm.addEventListener("submit", e => {
 
     const result = idx.search(input.value);
 
+    console.log(result) 
     result.forEach(({ ref }) => {
         const card = document.querySelector(`article[data-key='${ref}']`);
         card.classList.add('search-match')
