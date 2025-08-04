e88a6c3f2bab187a7c0f8a1fb188fae1559a9f6e
Will Denny <will@wjdenny.com>
2025-08-04 03:17:19 -0500

    search from url params

--- js/search.js
+++ js/search.js
@@ -1,35 +1,50 @@
-const searchForm = document.querySelector("form[role='search']")
+document.addEventListener('DOMContentLoaded', function () {
 
-const resources = document.querySelectorAll("#resources article")
-const data = [ ...resources ].map(e => e.dataset)
+    // Get the search terms from the URL
+    const params = (new URLSearchParams(window.location.search)).get("search")
+    const cards = [...document.querySelectorAll(".resource-card")]
 
-const idx = lunr(function () {
-    this.ref('key');
-    this.field('title');
-    this.field('note');
-    this.field('tags');
+    // Build an index to search
+    const data = cards.map(e => e.dataset)
 
-    this.k1(1.2);
-    this.b(0);
+    const idx = lunr(function () {
+        this.ref('key');
+        this.field('title');
+        this.field('note');
+        this.field('tags');
 
-    data.forEach(function (resource) { this.add(resource) }, this)
-})
+        this.k1(1.2);
+        this.b(0);
 
-searchForm.addEventListener("submit", e => { 
-    const input = e.target.querySelector("input[type='search'");
-    const cards = document.querySelectorAll(".resource-card");
-    [...cards].forEach(card => card.classList.remove('search-match'));
-
-    const result = idx.search(input.value);
-
-    console.log(result) 
-    result.forEach(({ ref }) => {
-        const card = document.querySelector(`article[data-key='${ref}']`);
-        card.classList.add('search-match')
+        data.forEach(function (resource) { this.add(resource) }, this)
     })
 
-    input.focus();
-    input.select();
+    // If there are search terms, 
+    if (params) {
+        // get the terms from the window location
+        const matches = idx.search(params).map(({ ref }) => ref)
 
-    e.preventDefault();
-})
+        // go through each resource card
+        cards.forEach(card => {
+            // remove any existing search-match class
+            card.classList.remove("search-match");
+
+            // add the search-match class if it has one of the matching keys
+            if (matches.includes(card.dataset.key)) {
+                card.classList.add("search-match")
+            }
+        })
+
+        // set the search input to the terms
+        const input = document.querySelector("form[role='search'] input[type='search']");
+        input.value = params;
+        input.focus();
+        input.select();
+    }
+
+    // If there are no search terms, display everything.
+    else {
+        cards.forEach(e => e.classList.remove("search-hidden"))
+    }
+
+});
